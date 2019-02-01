801148e10695508cd0d8d85af409ee8144644503
yonedahiroshi <yonedahiroshi@yonedahiroshinoMacBook-puro.local>
2019-02-01 12:10:10 +0900

    バリューオブジェクトとエンティティのサンプルコードを作成した。

--- Domain/Model/Entity/Parking.php
+++ Domain/Model/Entity/Parking.php
@@ -4,7 +4,61 @@
 namespace Domain\Model\Entity;
 
 
+use Domain\Model\ValueObject\ParkingId;
+use Domain\Model\ValueObject\ParkingPrice;
+
 class Parking
 {
+    private $id;
+
+    private $name;
+
+    private $priceDay;
+
+    private $priceTime;
+
+    public function __construct(ParkingId $parkingId)
+    {
+        $this->setId($parkingId);
+    }
+
+    public function setId(ParkingId $parkingId)
+    {
+        if ($this->id != null) {
+            throw new \InvalidArgumentException('すでに識別子が設定されています');
+        }
+
+        $this->id = $parkingId;
+    }
+
+    /**
+     * @param mixed $priceDay
+     */
+    public function setPriceDay(ParkingPrice $parkingPrice): void
+    {
+        $this->priceDay = $parkingPrice;
+    }
+
+    /**
+     * @param mixed $priceTime
+     */
+    public function setPriceTime(ParkingPrice $priceTime): void
+    {
+        if ($this->priceDay != null) {
+            throw new \InvalidArgumentException('まずは、日貸し料金を設定してください');
+        }
+
+        if (($priceTime->getPrice() * 4 * 12) > $this->priceDay->getPrice) {
+            throw new \InvalidArgumentException('日貸で借りた方が得な値にしてください');
+        }
+
+        $this->priceTime = $priceTime;
+    }
+
+
+
+
+
+
 
 }
