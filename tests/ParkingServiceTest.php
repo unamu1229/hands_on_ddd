efc44589d1147d37474f870cac4ca71da189d072
米田宏 <yonedahiroshi1229@gmail.com>
2019-02-05 23:00:03 +0900

    一覧に表示する駐車場が多くなった場合の負荷テスト

--- tests/ParkingServiceTest.php
+++ tests/ParkingServiceTest.php
@@ -35,4 +35,53 @@ class ParkingServiceTest extends TestCase
 
         $this->assertTrue($parkingList[0]->canParking());
     }
+
+
+    /**
+     * 負荷テスト
+     * 駐車場1000個、各駐車場に車室100の場合で1秒以下か
+     */
+    public function testParkingListCaseStress()
+    {
+        $timeStart = microtime(true);
+
+        $parkings = [];
+        for ($i = 0; $i < 1000; $i++) {
+            $parking = new Parking(new ParkingId(uniqid()));
+            $parking->setStatus(new ParkingStatus(ParkingStatus::ACTIVE));
+            $parkings[] = $parking;
+        }
+
+        $parkingSpaces = [];
+        for ($i = 0; $i < 100; $i++) {
+            $parkingSpace = new ParkingSpace(new ParkingSpaceId(uniqid()), $parking->getId());
+            if ($i == 99) {
+                $parkingSpace->setStatus(new ParkingSpaceStatus(ParkingSpaceStatus::ACTIVE));
+                $parkingSpaces[] = $parkingSpace;
+                continue;
+            }
+            $parkingSpace->setStatus(new ParkingSpaceStatus(ParkingSpaceStatus::STOP));
+            $parkingSpaces[] = $parkingSpace;
+        }
+
+
+        $mockParkingSpaceRepo = \Mockery::mock(ParkingSpaceRepository::class);
+        $mockParkingSpaceRepo->shouldReceive('findParkingSpace')->andReturn($parkingSpaces);
+
+        $mockParkingRepo = \Mockery::mock(ParkingRepository::class);
+        $mockParkingRepo->shouldReceive('all')->andReturn($parkings);
+
+        $parkingService = new ParkingService($mockParkingRepo, $mockParkingSpaceRepo);
+        $parkingList = $parkingService->parkingList();
+
+        $timeEnd = microtime(true);
+        $timeExecute = $timeEnd - $timeStart;
+
+        $this->assertTrue($timeExecute < 1, $timeExecute);
+
+        foreach ($parkingList as $parking) {
+            $this->assertTrue($parking->canParking());
+        }
+
+    }
 }
